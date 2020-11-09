316536bcb0d4e1fd812c8cb0bbaf4a4734c3d8f5
GitCaseyHub <41272778+GitCaseyHub@users.noreply.github.com>
2020-11-09 16:12:23 -0700

    Update ApproxProg.py
    Added function input

--- ApproxProg.py
+++ ApproxProg.py
@@ -1,32 +1,42 @@
 import matplotlib.pyplot as plt 
 import matplotlib
+from mpmath import *
   
-fig = plt.figure() 
-ax = fig.add_subplot(111) 
-  
-num_rect = int(input('Num rects in approx?'))
-bounds = input('Input bounds')
-lower_bound = int(bounds.split(',')[0][1:])
-upper_bound = int(bounds.split(',')[1][:-1])
-rect_distance = (upper_bound-lower_bound)/num_rect
-current_x = lower_bound
-counter=0
-coords = []
-sumApprox = 0
+def figurePlot():
+    fig = plt.figure() 
+    ax = fig.add_subplot(111) 
 
-for i in range(num_rect):
-    coords.append(current_x+rect_distance*counter)
-    counter+=1
-y = [x**2 for x in coords]
-coordCount=0
-for coord in range(num_rect):
-    ax.add_patch(matplotlib.patches.Rectangle((coords[coordCount],0),rect_distance,y[coordCount],color='red'))
-    sumApprox+=rect_distance*y[coordCount]
-    coordCount+=1
-        
-plt.xlim([lower_bound-5,upper_bound+5]) 
-plt.ylim([min(y)-5,max(y)+5]) 
-  
-plt.show() 
+    function = input('Input the function you\'d like to approximate the integral of: ')
+    num_rect = int(input('Input the number of rectangles you\'d like to approximate this integral with: '))
+    bounds = input('Input the lower and upper bounds of the integrable region in the form [a,b]: ')
+    lower_bound = int(bounds.split(',')[0][1:])
+    upper_bound = int(bounds.split(',')[1][:-1])
+    rect_distance = (upper_bound-lower_bound)/num_rect
+    current_x = lower_bound
+    counter=0
+    coords = []
+    sumApprox = 0
 
-print('Estimated Sum: '+str(sumApprox))
+    for i in range(num_rect):
+        coords.append(current_x+rect_distance*counter)
+        counter+=1
+    print(cleanup(function))
+    y = [float(eval(cleanup(function))) for x in coords]
+    coordCount=0
+    for coord in range(num_rect):
+        ax.add_patch(matplotlib.patches.Rectangle((coords[coordCount],0),rect_distance,y[coordCount],color='red'))
+        sumApprox+=rect_distance*y[coordCount]
+        coordCount+=1
+
+
+    plt.xlim([lower_bound-1,upper_bound+1]) 
+    plt.ylim([min(y)-1,max(y)+1]) 
+
+    plt.show() 
+
+    print('Estimated Sum: '+str(sumApprox))
+
+def cleanup(function):
+    if '^' in function:
+        function=function.replace('^','**')
+    return function

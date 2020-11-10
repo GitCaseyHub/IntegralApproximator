b1a41099f8a5cb43d4d66ac70c825d893cf8a80f
Casey <41272778+GitCaseyHub@users.noreply.github.com>
2020-11-09 23:30:28 -0700

    Update ApproxProg.py
    Update for better appearance of graphs given weird bounds.

--- ApproxProg.py
+++ ApproxProg.py
@@ -2,15 +2,16 @@ import matplotlib.pyplot as plt
 import matplotlib
 from mpmath import *
 import numpy as np
+import sys
   
 def figurePlot():
     fig = plt.figure() 
     ax = fig.add_subplot(111) 
 
-    function = input('Input the function you\'d like to approximate the integral of: ')
-    typeOfRule = input('What type of rule would you like to estimate the integral with? Righthand(R), Lefthand(L), or Midpoint(M)?: ')
-    num_rect = int(input('Input the number of rectangles you\'d like to approximate this integral with: '))
-    bounds = input('Input the lower and upper bounds of the integrable region in the form [a,b]: ')
+    function = input('What function would you like to estimate the integral of?\n')
+    typeOfRule = input('\nWhat type of rule would you like to estimate the integral with? Righthand(R), Lefthand(L), or Midpoint(M)?\n')
+    num_rect = int(input('\nHow many rectangles would you like to approximate the integral with?\n'))
+    bounds = input('\nInput the lower and upper bounds of the integrable region in the form [a,b]:\n')
     lower_bound = int(bounds.split(',')[0][1:])
     upper_bound = int(bounds.split(',')[1][:-1])
     rect_distance = (upper_bound-lower_bound)/num_rect
@@ -44,8 +45,17 @@ def figurePlot():
         sumApprox+=rect_distance*y[coordCount]
         coordCount+=1
 
-    plt.xlim(lower_bound-1,upper_bound+1) 
-    plt.ylim(min(y)-1,max(y)+1)
+    plt.xlim(lower_bound -(0.05)*abs(lower_bound),upper_bound + 0.05*abs(upper_bound)) 
+    
+    if max(y) + 1 < 0:
+        plt.ylim(min(y)-1,0.5)
+        
+    elif min(y)-1 > 0:
+        plt.ylim(-0.5,max(y)+1)
+        
+    else:
+        plt.ylim(min(y)-1,max(y)+1)
+        
     plt.ylabel('Y Values')
     plt.xlabel('X Values')
     plt.plot(x_points,y_points)
@@ -53,6 +63,17 @@ def figurePlot():
     plt.show() 
 
     print('Estimated Sum using '+typeOfRule+' rule: '+str(sumApprox))
+    repeat = input('Would you like to estimate another function?')
+    
+    if repeat==('Yes' or 'Y' or 'y' or 'yes'):
+        figurePlot()
+        
+    else:
+        print('Thank you and goodbye.')
+        try:
+            sys.exit()
+        except:
+            pass
 
 def cleanup(function):
     if '^' in function:
